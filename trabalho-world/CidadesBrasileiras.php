6a1d48051e976389b0bdb2d2951d0443cf6c1b4a
LHenriqueBrito <60859236+LHenriqueBrito@users.noreply.github.com>
2021-05-23 20:19:36 -0300

    Update CidadesBrasileiras.php

--- trabalho-world/CidadesBrasileiras.php
+++ trabalho-world/CidadesBrasileiras.php
@@ -13,7 +13,7 @@
 EOT;
 ?>
 <h1>Cidades brasileiras</h1>
-<table class="table table-striped table-sm table-bordered">
+<table class="table table-striped table-hover">
 	<thead>
 		<tr>
 		<th scope="col">Nome</th>
